e088ce158216985f697f2e0b05dc985c09aff327
marlagan <martynaa.laganowska@gmail.com>
2025-04-20 12:13:49 +0200

    Update Key.java

--- src/main/java/Model/Key.java
+++ src/main/java/Model/Key.java
@@ -12,7 +12,7 @@ public class Key {
      */
     public ArrayList<byte[]> generateSubKeys(byte[] key) {
         Data data = new Data();
-        byte[] key56 = data.permute(PC1, key); //usuwamy bity parzystosci poprzez permutacje z pominięciem tych bitów, więc otrzymujemy 56 bity z 64 bitów klucza
+        byte[] key56 = data.permute(PC1, key); 
         ArrayList<byte[]> subKeys = new ArrayList<>();
         int half = key56.length / 2;
         byte[] key28L = new byte[half];
@@ -20,15 +20,15 @@ public class Key {
         System.arraycopy(key56, 0, key28L, 0, half);
         System.arraycopy(key56, half, key28R, 0, half);
 
-        for (int i = 0; i < 16; i++) { //dla rund 1,2,9 i 16 rotujemy raz, dla pozostałych rotujemy 2
+        for (int i = 0; i < 16; i++) { 
 
             key28L = shiftLeft(key28L, shiftBits[i]);
             key28R = shiftLeft(key28R, shiftBits[i]);
 
-            System.arraycopy(key28L, 0, key56, 0, half); //łączenie spowrotem do jednej tablicy
+            System.arraycopy(key28L, 0, key56, 0, half);
             System.arraycopy(key28R, 0, key56, half, half);
 
-            subKeys.add(data.permute(PC2, key56)); //permutacja 56 bitow klucza -> 48 bitów każdy podklucz
+            subKeys.add(data.permute(PC2, key56)); 
         }
 
         return subKeys;
@@ -40,24 +40,16 @@ public class Key {
      * @param shiftAmount amount of shifts we have to do
      * @return altered bits
      */
-    private byte[] shiftLeft(byte[] bits, int shiftAmount){ //przesunięcie powinno być na bitach, a nie na bajtach (?)
-
-        //to akurat głównie wypluł czat, ale trochę nie ogarniam tych przesunięć, więc pewnie poprawię zaraz:
-
-        int bitField = 0; // Zmienna przechowująca 28 bitów jako liczba całkowita
-
-        // Łączenie bajtów w jedną liczbę 28-bitową
+    private byte[] shiftLeft(byte[] bits, int shiftAmount){ 
+        
+        int bitField = 0; 
         for (int i = 0; i < bits.length; i++) {
             bitField = (bitField << 1) | (bits[i] & 1);
         }
 
-        // Usunięcie zbędnych bitów poza 28-bitowym zakresem
+
         bitField &= 0x0FFFFFFF;
-
-        // Przesunięcie bitowe z zawinięciem (cykliczny shift)
         bitField = ((bitField << shiftAmount) | (bitField >>> (28 - shiftAmount))) & 0x0FFFFFFF;
-
-        // Konwersja z powrotem na tablicę bajtów
         byte[] newBits = new byte[bits.length];
         for (int i = bits.length - 1; i >= 0; i--) {
             newBits[i] = (byte) (bitField & 0xFF);
